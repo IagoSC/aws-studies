930023cf5fd3abb0411ea1a7b55281e034301ce8
iago de sousa <iago.sousa@leanonsystems.com>
2022-08-30 18:33:31 -0300

    Configuration, Basic of stack First Handler

---
+++ lib/productsApp-stack.ts
@@ -0,0 +1,29 @@
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as cdk from "aws-cdk-lib"
+import { Construct } from "constructs"
+
+export class ProductsAppStack extends cdk.Stack {
+  
+  readonly fetchProductsHandler
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps){
+    super(scope, id, props)
+
+    this.fetchProductsHandler = new lambdaNodeJS.NodejsFunction(
+      this,
+      "ProductsFetchFunction",
+      {
+        functionName: "ProductsFetchFunction",
+        entry: "lambda/products/ProductsFetchFunction.ts",
+        handler: "handler",
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(2),
+        bundling: {
+          minify: true,
+          sourceMap: false
+        }
+      }
+    )
+  }
+}
